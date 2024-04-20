f364fc8bb08621fce623d1d266291574295dc058
Unchana19 <annchanaoat@gmail.com>
2024-04-20 23:56:14 +0700

    add icon

--- frontend/src/components/HealthBlogComponent.jsx
+++ frontend/src/components/HealthBlogComponent.jsx
@@ -7,8 +7,8 @@ export default function HealthBlogComponent({ id, title, subTitle, image }) {
         <Link className="w-full h-[300px] col-span-12 sm:col-span-6" to={`/Health-Information/${id}`}>
             <Card isFooterBlurred className="w-full h-[300px]">
                 <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">Blog</p>
-                    <h4 className="text-white font-medium text-xl">{title}</h4>
+                    <p className="text-tiny text-white/60 uppercase font-bold">Health Information</p>
+                    <h4 className="text-green-200 font-medium text-xl">{title}</h4>
                 </CardHeader>
                 <Image
                     isZoomed
@@ -21,7 +21,7 @@ export default function HealthBlogComponent({ id, title, subTitle, image }) {
                     <div className="flex flex-grow gap-2 items-center">
                         <p className="text-tiny text-white/60">{subTitle}</p>
                     </div>
-                    <Button radius="full" size="sm">อ่านเพิ่มเติม</Button>
+                    <Button color="success" radius="full" size="sm">อ่านเพิ่มเติม</Button>
                 </CardFooter>
             </Card>
         </Link>
