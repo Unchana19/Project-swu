c71868bf8f72cddd012a9ac8fc1bf0e60851d250
Unchana19 <annchanaoat@gmail.com>
2024-04-20 23:44:18 +0700

    hash router

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button, Avatar } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Button, Avatar } from "@nextui-org/react";
 import { getSession } from "../services/authorize";
-import {useNavigate} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavbarComponent() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const pathName = window.location.pathname;
-  const navigate = useNavigate();
+  const location = useLocation();
 
   const menuItems = [
-    "BMI",
-    "Health Information",
-    "Community",
-    "Log Out",
-  ];
-
-  const linkItem = [
-    "/",
-    "/Health-Information",
-    "/Community",
-    "/Profile",
+    { label: "BMI", path: "/" },
+    { label: "Health Information", path: "/Health-Information" },
+    { label: "Community", path: "/Community" },
+    { label: `${getSession("username") ? "Logout" : "Login"}`, path: `${getSession("username") ? "/Profile" : "/Login"}` },
   ];
 
   return (
@@ -44,18 +36,18 @@ export default function NavbarComponent() {
           <p className="font-bold text-inherit text-white">LOGO</p>
         </NavbarBrand>
         <NavbarItem>
-          <Link href="/" aria-current={pathName === "/" ? "page" : undefined}>
-            <p className={pathName === "/" ? "text-green-500" : "text-white"}>BMI</p>
+          <Link to="/" aria-current={location.pathname === "/" ? "page" : undefined}>
+            <p className={location.pathname === "/" ? "text-green-500" : "text-white"}>BMI</p>
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="/Health-Information" aria-current={pathName === "/Health-Information" ? "page" : undefined}>
-            <p className={pathName === "/Health-Information" ? "text-green-500" : "text-white"}>Health Information</p>
+          <Link to="/Health-Information" aria-current={location.pathname === "/Health-Information" ? "page" : undefined}>
+            <p className={location.pathname === "/Health-Information" ? "text-green-500" : "text-white"}>Health Information</p>
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="/Community" aria-current={pathName === "/Community" ? "page" : undefined}>
-            <p className={pathName === "/Community" ? "text-green-500" : "text-white"}>Community</p>
+          <Link to="/Community" aria-current={location.pathname === "/Community" ? "page" : undefined}>
+            <p className={location.pathname === "/Community" ? "text-green-500" : "text-white"}>Community</p>
           </Link>
         </NavbarItem>
       </NavbarContent>
@@ -64,14 +56,14 @@ export default function NavbarComponent() {
         <NavbarItem className="hidden sm:flex">
           {getSession("username") ?
             <p className="text-white">{getSession("username")}</p> :
-            <Link href="/Sign-Up"><p className="text-white">Sign up</p></Link>}
+            <Link to="/Sign-Up"><p className="text-white">Sign up</p></Link>}
         </NavbarItem>
         <NavbarItem className="items-center justify-center flex">
           {getSession("username") ?
-            <Link href="/Profile">
+            <Link to="/Profile">
               <Avatar showFallback src='https://images.unsplash.com/broken' />
             </Link> :
-            <Button as={Link} color="success" href="/Login" variant="flat">
+            <Button as={Link} color="success" to="/Login" variant="flat">
               Login
             </Button>}
         </NavbarItem>
@@ -79,16 +71,12 @@ export default function NavbarComponent() {
 
       <NavbarMenu>
         {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
+          <NavbarMenuItem key={`${item.label}-${index}`}>
             <Link
-              className="w-full"
-              color={
-                pathName === linkItem[index] ? "success" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              onPress={() => navigate(linkItem[index])}
-              size="lg"
+              className={`w-full ${location.pathname === item.path ? "text-green-500" : menuItems.length - 1 === index && getSession("username") ? "text-rose-700" : ""}`}
+              to={item.path}
             >
-              {item}
+              {item.label}
             </Link>
           </NavbarMenuItem>
         ))}
